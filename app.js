cc5f98e56a98bf3156cc4156b1f8852047013e1f
Lucas Moraes <lucas.moraes@sptech.school>
2026-08-25 18:40:41 -0300

    feat: .env examples | api structure

---
+++ app.js
@@ -0,0 +1,29 @@
+// const ambiente_processo = 'producao';
+const ambiente_processo = "desenvolvimento";
+
+const caminho_env = ambiente_processo === "producao" ? ".env" : ".env.dev";
+
+require("dotenv").config({ path: caminho_env });
+
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const PORTA_APP = process.env.APP_PORT;
+const HOST_APP = process.env.APP_HOST;
+
+const app = express();
+
+const indexRouter = require("./src/routes/index");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+app.use("/", indexRouter);
+
+
+app.listen(PORTA_APP, function () {
+	console.log(
+    `Servidor rodando: http://${HOST_APP}:${PORTA_APP}`)
+});
