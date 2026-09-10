263b64a8616c2d4c48357cb0f20bcf1e7149431b
Nicole Miranda <nicole@nicolemiranda.nicolemiranda>
2026-09-10 01:14:39 -0300

    Feat: Ajustando Front end da página home e da página equipe

--- src/database/config.js
+++ src/database/config.js
@@ -1,5 +1,14 @@
 const mysql = require("mysql2/promise");
 
+
+console.log("DEBUG →", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: process.env.DB_PORT,
+});
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
