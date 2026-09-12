46c9eebfd751ad1c30c70ef3ae19840808a33cc1
Lucas Moraes <lucas.moraes@sptech.school>
2026-09-12 20:42:40 -0300

    refactor: simplificando a função de bloquear a aside

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -1,28 +1,7 @@
-getPapel()
-async function getPapel(){
-    const emailUsuario = sessionStorage.EMAIL_USUARIO
-    const idUsuario = sessionStorage.ID_USUARIO
-    
-    const response = await fetch("/employee/getPapel", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                usuarioId: idUsuario,
-                usuarioEmail: emailUsuario
-            })
-        })
-
-        if (!response.ok) {
-            throw new Error(`HTTP ${response.status}`)
-        }
-
-        const papel = await response.json()
-        gerarAside(papel)
-}
 let cadastrarFunc = document.getElementById("CadastrarFuncionario")
 let verFunc = document.getElementById("VerFuncionarios")
+
+
 function gerarAside(papel){
     console.log(papel[0]["papel"])
     if(papel[0]["papel"] != 1){
@@ -30,3 +9,7 @@ function gerarAside(papel){
         verFunc.style.display="none";
     }
 }
+
+gerarAside(sessionStorage.ID_PAPEL)
+
+
